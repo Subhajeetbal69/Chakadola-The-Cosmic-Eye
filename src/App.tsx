71b134851f72e185909e8a17e49593def1fc062d
rigvedsaimaharathi <rigvedsaimaharathi@gmail.com>
2026-08-22 22:22:24 +0530

    app file change_ functions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-npm installimport React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import {
   Header
 } from './components/Header';
